64790b3b0d681e99d3a998d84f484439ae6d95da
mm-110 <massimomontanaro.1993@gmail.com>
2024-11-18 19:27:39 +0100

    Initial Commit

---
+++ Articles/ArticlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticlesApp.swift
+//  Articles
+//
+//  Created by Massimo Montanaro on 18/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ArticlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArticlesTests/ArticlesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArticlesTests.swift
+//  ArticlesTests
+//
+//  Created by Massimo Montanaro on 18/11/24.
+//
+
+import Testing
+@testable import Articles
+
+struct ArticlesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
